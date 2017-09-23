0c3c6202de31bcee766254b77f5f34907d544f1e
misspia <pia_leung@hotmail.com>
2017-09-23 19:35:13 -0400

    nav layout, styles, hover effects

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import Nav from './nav/nav.jsx';
 import Landing from './landing/landing.jsx';
 import Services from './services/services.jsx';
 import Portfolio from './portfolio/portfolio.jsx';
@@ -11,6 +12,7 @@ import './index.scss';
 class App extends Component {
 	render() {
 		return <div className="main">
+			<Nav />
 			<Landing />
 			<Services />
 		</div>;
