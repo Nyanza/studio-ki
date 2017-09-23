80e3518f53749f4af47c2643cc6ee10eced5d97d
misspia <pia_leung@hotmail.com>
2017-09-23 10:24:30 -0400

    configure webpack to build react

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.scss';
+
+class App extends Component {
+	render() {
+		return <div className="main">
+			supp
+		</div>;
+	}
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+);
+
