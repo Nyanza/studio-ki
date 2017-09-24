88d5e35fcc413d3683fb52132045832a6238bbd1
misspia <pia_leung@hotmail.com>
2017-09-23 23:10:33 -0400

    contacts layout, xore styling, form setup

---
+++ src/contact/metadata.js
@@ -0,0 +1,9 @@
+const Metadata = {
+	title: 'contact',
+	description: 'drop us a message',
+	senderField: 'name',
+	messageField: 'stuff you wanna say',
+	submit: 'send'
+};
+
+export default Metadata;
